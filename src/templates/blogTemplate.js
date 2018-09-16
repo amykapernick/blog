c0c904c624b6c8107097d1283561b1b3930edef8
Amy Kapernick <amy@aimhigherwebdesign.com>
2018-09-16 14:34:25 +0800

    changed format for blog

--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -3,13 +3,12 @@ import { graphql } from "gatsby"
 
 export default function Template({data}) {
     const { markdownRemark } = data // data.markdownRemark holds our post data
-    const { frontmatter, html } = markdownRemark
+    const { frontmatter } = markdownRemark
     return (
         <div className="blog-post-container">
             <div className="blog-post">
                 <h1>{frontmatter.title}</h1>
                 <h2>{frontmatter.publishDate}</h2>
-                <div dangerouslySetInnerHTML={{ __html: html }}/>
             </div>
         </div>
     )
@@ -18,10 +17,8 @@ export default function Template({data}) {
 export const pageQuery = graphql`
     query($path: String!) {
         markdownRemark(frontmatter: { path: { eq: $path } }) {
-            html
             frontmatter {
                 publishDate(formatString: "MMMM DD, YYYY")
-                path
                 title
             }
         }
