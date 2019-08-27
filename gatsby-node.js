02f7cbb4544a3daac5d05c65ebbc7caad4a86b3c
Amy Kapernick <amy@aimhigherwebdesign.com>
2019-08-27 12:04:23 +0800

    Updated for draft status

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,10 +1,10 @@
-const _ = require('lodash')
-const path = require('path')
-const { createFilePath } = require('gatsby-source-filesystem')
-const { fmImagesToRelative } = require('gatsby-remark-relative-images');
+const _ = require("lodash");
+const path = require("path");
+const { createFilePath } = require("gatsby-source-filesystem");
+const { fmImagesToRelative } = require("gatsby-remark-relative-images");
 
 exports.createPages = ({ actions, graphql }) => {
-  const { createPage } = actions
+  const { createPage } = actions;
 
   return graphql(`
     {
@@ -15,44 +15,47 @@ exports.createPages = ({ actions, graphql }) => {
             fields {
               slug
             }
+            frontmatter {
+              draft
+            }
           }
         }
       }
     }
   `).then(result => {
     if (result.errors) {
-      result.errors.forEach(e => console.error(e.toString()))
-      return Promise.reject(result.errors)
+      result.errors.forEach(e => console.error(e.toString()));
+      return Promise.reject(result.errors);
     }
 
-    const posts = result.data.allMarkdownRemark.edges
+    const posts = result.data.allMarkdownRemark.edges;
 
     posts.forEach(edge => {
-      const id = edge.node.id
-      createPage({
-        path: edge.node.fields.slug.replace('/blog/posts', ''),
-        component: path.resolve(
-          `src/templates/blogTemplate.js`
-        ),
-        // additional data can be passed via context
-        context: {
-          id,
-        },
-      })
-    })
-  })
-}
+      const id = edge.node.id;
+      if (!edge.node.frontmatter.draft) {
+        createPage({
+          path: edge.node.fields.slug.replace("/blog/posts", ""),
+          component: path.resolve(`src/templates/blogTemplate.js`),
+          // additional data can be passed via context
+          context: {
+            id
+          }
+        });
+      }
+    });
+  });
+};
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
-  const { createNodeField } = actions
+  const { createNodeField } = actions;
   fmImagesToRelative(node);
 
   if (node.internal.type === `MarkdownRemark`) {
-    const value = createFilePath({ node, getNode })
+    const value = createFilePath({ node, getNode });
     createNodeField({
       name: `slug`,
       node,
-      value,
-    })
+      value
+    });
   }
-}
+};
