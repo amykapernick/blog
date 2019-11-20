f51ead3fe8c411cefae48f560129aae4cd2c363d
Amy Kapernick <amy@aimhigherweb.design>
2019-11-20 09:27:48 +0800

    added linting and react-axe

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,91 +1,104 @@
 module.exports = {
-  siteMetadata: {
-    title: "Amy Goes to Perth",
-    description:
-      "Freelance web developer who lives in Perth with her border collie. Just a few musings and ramblings...",
-    siteUrl: "https://amygoestoperth.com.au"
-  },
-  plugins: [
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sass",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        path: `${__dirname}/src/blog/img`,
-        name: "uploads"
-      }
-    },
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        path: `${__dirname}/src/img`,
-        name: "images"
-      }
-    },
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/src/blog/posts`,
-        name: "markdown-pages"
-      }
-    },
-    {
-      resolve: `gatsby-plugin-google-tagmanager`,
-      options: {
-        id: "GTM-MDKC4WR",
-        includeInDevelopment: true
-      }
-    },
+	siteMetadata: {
+		title: 'Amy Goes to Perth',
+		description: 'Freelance web developer who lives in Perth with her border collie. Just a few musings and ramblings...',
+		siteUrl: 'https://amygoestoperth.com.au',
+	},
+	plugins: [
+		'gatsby-plugin-react-helmet',
+		'gatsby-plugin-sass',
+		{
+			resolve: 'gatsby-plugin-react-axe',
+			options: {
+				// Integrate react-axe in production. This defaults to false.
+				showInProduction: false,
 
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        plugins: [
-          {
-            resolve: `@raae/gatsby-remark-oembed`
-          },
-          {
-            resolve: `gatsby-remark-autolink-headers`,
-            options: {
-              offsetY: `100`,
-              icon: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-link"><path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71"></path><path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71"></path></svg>`,
-              maintainCase: false,
-              removeAccents: true,
-              enableCustomId: true
-            }
-          },
-          `gatsby-remark-relative-images`,
-          {
-            resolve: `gatsby-remark-images`,
-            options: {
-              showCaptions: true,
-              maxWidth: 1000,
-              widthWebp: true
-            }
-          },
-          {
-            resolve: `gatsby-remark-prismjs`,
-            options: {
-              classPrefix: "language-",
-              // This lets you set up language aliases.  For example, setting this to '{ sh: "bash" }' will let you use the language "sh" which will highlight using the bash highlighter.
-              aliases: {},
-              showLineNumbers: false,
-              noInlineHighlight: true,
-              // This adds a new language definition to Prism or extend an already existing language definition. More details on this option can be found under the header "Add new language definition or extend an existing language" below.
-              languageExtensions: [],
-              // Customize the prompt used in shell output
-              prompt: {
-                user: "me",
-                host: "amyskapers.dev",
-                global: true
-              }
-            }
-          }
-        ]
-      }
-    },
-    "gatsby-plugin-netlify-cms"
-  ]
-};
+				// Options to pass to axe-core.
+				// See: https://github.com/dequelabs/axe-core/blob/master/doc/API.md#api-name-axeconfigure
+				axeOptions: {
+					reporter: 'v2',
+				},
+			},
+		},
+		{
+			resolve: 'gatsby-source-filesystem',
+			options: {
+				path: `${__dirname}/src/blog/img`,
+				name: 'uploads',
+			},
+		},
+		{
+			resolve: 'gatsby-source-filesystem',
+			options: {
+				path: `${__dirname}/src/img`,
+				name: 'images',
+			},
+		},
+		'gatsby-plugin-sharp',
+		'gatsby-transformer-sharp',
+		{
+			resolve: 'gatsby-source-filesystem',
+			options: {
+				path: `${__dirname}/src/blog/posts`,
+				name: 'markdown-pages',
+			},
+		},
+		{
+			resolve: 'gatsby-plugin-google-tagmanager',
+			options: {
+				id: 'GTM-MDKC4WR',
+				includeInDevelopment: true,
+			},
+		},
+
+		{
+			resolve: 'gatsby-transformer-remark',
+			options: {
+				plugins: [
+					{
+						resolve: '@raae/gatsby-remark-oembed',
+					},
+					{
+						resolve: 'gatsby-remark-autolink-headers',
+						options: {
+							offsetY: '100',
+							icon:
+								'<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-link"><path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71"></path><path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71"></path></svg>',
+							maintainCase: false,
+							removeAccents: true,
+							enableCustomId: true,
+						},
+					},
+					'gatsby-remark-relative-images',
+					{
+						resolve: 'gatsby-remark-images',
+						options: {
+							showCaptions: true,
+							maxWidth: 1000,
+							widthWebp: true,
+						},
+					},
+					{
+						resolve: 'gatsby-remark-prismjs',
+						options: {
+							classPrefix: 'language-',
+							// This lets you set up language aliases.  For example, setting this to '{ sh: "bash" }' will let you use the language "sh" which will highlight using the bash highlighter.
+							aliases: {},
+							showLineNumbers: false,
+							noInlineHighlight: true,
+							// This adds a new language definition to Prism or extend an already existing language definition. More details on this option can be found under the header "Add new language definition or extend an existing language" below.
+							languageExtensions: [],
+							// Customize the prompt used in shell output
+							prompt: {
+								user: 'me',
+								host: 'amyskapers.dev',
+								global: true,
+							},
+						},
+					},
+				],
+			},
+		},
+		'gatsby-plugin-netlify-cms',
+	],
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,164 +1,131 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { graphql, Link } from "gatsby";
-import Layout from "../components/layout";
-import Img from "gatsby-image";
+import React from 'react'
+import { graphql, Link } from 'gatsby'
+import Layout from '../components/layout'
+import Img from 'gatsby-image'
 
-import "../scss/layouts/feed.scss";
-import { Facebook, Twitter } from "react-feather";
-import AmyKate from "../img/amykate.jpg";
-import AimHigher from "../img/aimhigher.png";
-import Freelance from "../img/freelancers.png";
+import '../scss/layouts/feed.scss'
+import { Facebook, Twitter } from 'react-feather'
+import AmyKate from '../img/amykate.jpg'
+import AimHigher from '../img/aimhigher.png'
+import Freelance from '../img/freelancers.png'
 
 const profiles = {
-  "Amy Goes To Perth": {
-    title: "Amy Goes to Perth",
-    id: "amykate",
-    image: AmyKate,
-    url: "https://amygoestoperth.com.au/"
-  },
-  AimHigher: {
-    title: "AimHigher Web Design",
-    id: "aimhigher",
-    image: AimHigher,
-    url: "https://aimhigherwebdesign.com.au/"
-  },
-  "The Freelance Guide": {
-    title: "Freelancer's Guide",
-    id: "freelance",
-    image: Freelance,
-    url: "https://thefreelance.guide/"
-  }
-};
-
-export default class IndexPage extends React.Component {
-  render() {
-    const { data } = this.props;
-    const { edges: posts } = data.allMarkdownRemark;
-
-    const meta = {
-      name: data.site.siteMetadata.title,
-      description: data.site.siteMetadata.description,
-      slug: data.site.siteMetadata.siteUrl
-    };
-
-    return (
-      <Layout meta={meta}>
-        <div className="article-feed">
-          {posts.map(({ node: post }) => {
-            if (new Date(post.frontmatter.publishDate) > new Date()) {
-              return;
-            }
-            let author = profiles[post.frontmatter.mainBlog],
-              articleLink = author.url + post.fields.slug,
-              facebookLink =
-                "https://www.facebook.com/sharer/sharer.php?u=" + articleLink,
-              twitterLink =
-                "https://twitter.com/home?status=So%20%40amys_kapers%20wrote%20this%20really%20cool%20blog%20post,%20you%20should%20check%20it%20out!%20" +
-                articleLink;
-
-            return (
-              <article id={post.id} key={post.id} className="feed-article">
-                <Img
-                  className="image-feature"
-                  fixed={post.frontmatter.featuredImage.childImageSharp.fixed}
-                />
-                <div className="author">
-                  <div className="image-profile">
-                    {author.url !== "" ? (
-                      <a
-                        href={author.url}
-                        target="_blank"
-                        rel="nofollow"
-                        title={"Link to host blog, " + author.title}
-                      >
-                        <img alt="Profile Image" src={author.image} />
-                      </a>
-                    ) : (
-                      <img alt="Profile Image" src={author.image} />
-                    )}
-                  </div>
-                </div>
-                <header>
-                  <h2 className="article-title">
-                    <Link
-                      id={post.id}
-                      to={`/${post.fields.slug.replace("/blog/posts", "")}`}
-                    >
-                      {post.frontmatter.title}
-                    </Link>
-                  </h2>
-                  <h6 className="date">{post.frontmatter.publishDate}</h6>
-                </header>
-                <div className="excerpt">{post.excerpt}</div>
-                <div className="share-icons">
-                  <a
-                    href={facebookLink}
-                    target="_blank"
-                    className="facebook share-link"
-                  >
-                    {<Facebook />}
-                  </a>
-                  <a
-                    href={twitterLink}
-                    target="_blank"
-                    className="twitter share-link"
-                  >
-                    {<Twitter />}
-                  </a>
-                </div>
-              </article>
-            );
-          })}
-        </div>
-      </Layout>
-    );
-  }
+	'Amy Goes To Perth': {
+		title: 'Amy Goes to Perth',
+		id: 'amykate',
+		image: AmyKate,
+		url: 'https://amygoestoperth.com.au/',
+	},
+	AimHigher: {
+		title: 'AimHigher Web Design',
+		id: 'aimhigher',
+		image: AimHigher,
+		url: 'https://aimhigherwebdesign.com.au/',
+	},
+	'The Freelance Guide': {
+		title: "Freelancer's Guide",
+		id: 'freelance',
+		image: Freelance,
+		url: 'https://thefreelance.guide/',
+	},
 }
 
-IndexPage.propTypes = {
-  data: PropTypes.shape({
-    allMarkdownRemark: PropTypes.shape({
-      edges: PropTypes.array
-    })
-  })
-};
+export default class IndexPage extends React.Component {
+	render() {
+		const { data } = this.props,
+			{ edges: posts } = data.allMarkdownRemark,
+			meta = {
+				name: data.site.siteMetadata.title,
+				description: data.site.siteMetadata.description,
+				slug: data.site.siteMetadata.siteUrl,
+			}
+
+		return (
+			<Layout meta={meta}>
+				<div className="article-feed">
+					{posts.map(({ node: post }) => {
+						if (new Date(post.frontmatter.publishDate) > new Date()) {
+							return
+						}
+						const author = profiles[post.frontmatter.mainBlog],
+							articleLink = author.url + post.fields.slug,
+							facebookLink = 'https://www.facebook.com/sharer/sharer.php?u=' + articleLink,
+							twitterLink =
+								'https://twitter.com/home?status=So%20%40amys_kapers%20wrote%20this%20really%20cool%20blog%20post,%20you%20should%20check%20it%20out!%20' +
+								articleLink
+
+						return (
+							<article id={post.id} key={post.id} className="feed-article">
+								<Img className="image-feature" fixed={post.frontmatter.featuredImage.childImageSharp.fixed} />
+								<div className="author">
+									<div className="image-profile">
+										{author.url !== '' ? (
+											<a href={author.url} target="_blank" rel="nofollow" title={'Link to host blog, ' + author.title}>
+												<img alt="Profile Image" src={author.image} />
+											</a>
+										) : (
+											<img alt="Profile Image" src={author.image} />
+										)}
+									</div>
+								</div>
+								<header>
+									<h2 className="article-title">
+										<Link id={post.id} to={`${post.fields.slug.replace('/blog/posts', '')}`}>
+											{post.frontmatter.title}
+										</Link>
+									</h2>
+									<h6 className="date">{post.frontmatter.publishDate}</h6>
+								</header>
+								<div className="excerpt">{post.excerpt}</div>
+								<div className="share-icons">
+									<a href={facebookLink} target="_blank" className="facebook share-link">
+										{<Facebook />}
+									</a>
+									<a href={twitterLink} target="_blank" className="twitter share-link">
+										{<Twitter />}
+									</a>
+								</div>
+							</article>
+						)
+					})}
+				</div>
+			</Layout>
+		)
+	}
+}
 
 export const pageQuery = graphql`
-  query IndexQuery {
-    site {
-      siteMetadata {
-        title
-        description
-        siteUrl
-      }
-    }
-    allMarkdownRemark(
-      sort: { order: DESC, fields: [frontmatter___updateDate] }
-      filter: { frontmatter: { draft: { ne: true } } }
-    ) {
-      edges {
-        node {
-          id
-          excerpt(pruneLength: 400)
-          fields {
-            slug
-          }
-          frontmatter {
-            title
-            publishDate(formatString: "DD MMM YYYY")
-            mainBlog
-            tags
-            featuredImage {
-              childImageSharp {
-                fixed(width: 500) {
-                  ...GatsbyImageSharpFixed_withWebp
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-`;
+	query IndexQuery {
+		site {
+			siteMetadata {
+				title
+				description
+				siteUrl
+			}
+		}
+		allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___updateDate] }, filter: { frontmatter: { draft: { ne: true } } }) {
+			edges {
+				node {
+					id
+					excerpt(pruneLength: 400)
+					fields {
+						slug
+					}
+					frontmatter {
+						title
+						publishDate(formatString: "DD MMM YYYY")
+						mainBlog
+						tags
+						featuredImage {
+							childImageSharp {
+								fixed(width: 500) {
+									...GatsbyImageSharpFixed_withWebp
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+`
