86687a009dcf913dbb82fb8d26b2dfc179244c1c
Amy Kapernick <amy@aimhigherweb.design>
2019-11-21 08:18:16 +0800

    adde basic a11y test using pa11y

--- tests/basic-a11y.js
+++ tests/basic-a11y.js
@@ -1,22 +1,61 @@
 const pa11y = require('pa11y'),
 	fs = require('file-system')
 
-pa11y('http://localhost:8000/', {
-	standard: 'WCAG2AA',
-	actions: [
-		`screen capture ${__dirname}/results/basic-a11y_home.png`,
-		'navigate to http://localhost:8000/template',
-		`screen capture ${__dirname}/results/basic-a11y_post.png`,
-	],
-	// Log what's happening to the console
-	screenCapture: `${__dirname}/results/basic-a11y.png`,
-	// Options go here
-})
-	.then(async results => {
+runTest()
+
+async function runTest() {
+	try {
+		const results = await Promise.all([
+			pa11y('http://localhost:8000', {
+				standard: 'WCAG2AA',
+				actions: [],
+				screenCapture: `screen capture ${__dirname}/results/basic-a11y_home_mobile.png`,
+				viewport: {
+					width: 320,
+					height: 480,
+					deviceScaleFactor: 2,
+					isMobile: true,
+				},
+			}),
+			pa11y('http://localhost:8000', {
+				standard: 'WCAG2AA',
+				actions: [],
+				screenCapture: `screen capture ${__dirname}/results/basic-a11y_home_desktop.png`,
+				viewport: {
+					width: 1280,
+					height: 1024,
+					deviceScaleFactor: 1,
+					isMobile: false,
+				},
+			}),
+			pa11y('http://localhost:8000/setting-up-a-windows-computer-for-dev', {
+				standard: 'WCAG2AA',
+				actions: [],
+				screenCapture: `screen capture ${__dirname}/results/basic-a11y_post_mobile.png`,
+				viewport: {
+					width: 320,
+					height: 480,
+					deviceScaleFactor: 2,
+					isMobile: true,
+				},
+			}),
+			pa11y('http://localhost:8000/setting-up-a-windows-computer-for-dev', {
+				standard: 'WCAG2AA',
+				actions: [],
+				screenCapture: `screen capture ${__dirname}/results/basic-a11y_post_desktop.png`,
+				viewport: {
+					width: 1280,
+					height: 1024,
+					deviceScaleFactor: 1,
+					isMobile: false,
+				},
+			}),
+		])
+
 		fs.writeFile('tests/results/basic-a11y.json', JSON.stringify(results), err => {
 			console.log(err)
 		})
-	})
-	.catch(err => {
-		console.log(err)
-	})
+	} catch (err) {
+		console.error(err.message)
+	}
+}
