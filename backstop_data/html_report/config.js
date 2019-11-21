e9fd31b8a9c898d3aa0ed9d695937d20a48a6381
Amy Kapernick <amy@aimhigherweb.design>
2019-11-21 18:29:01 +0800

    updated backstop test

--- backstop_data/html_report/config.js
+++ backstop_data/html_report/config.js
@@ -4,7 +4,7 @@ report({
     {
       "pair": {
         "reference": "..\\bitmaps_reference\\backstop_default_Blog_Homepage_0_document_0_phone.png",
-        "test": "..\\bitmaps_test\\20191121-181548\\backstop_default_Blog_Homepage_0_document_0_phone.png",
+        "test": "..\\bitmaps_test\\20191121-182735\\backstop_default_Blog_Homepage_0_document_0_phone.png",
         "selector": "document",
         "fileName": "backstop_default_Blog_Homepage_0_document_0_phone.png",
         "label": "Blog Homepage",
@@ -20,16 +20,17 @@ report({
             "width": 0,
             "height": 0
           },
-          "misMatchPercentage": "0.01",
-          "analysisTime": 654
-        }
+          "misMatchPercentage": "0.58",
+          "analysisTime": 643
+        },
+        "diffImage": "..\\bitmaps_test\\20191121-182735\\failed_diff_backstop_default_Blog_Homepage_0_document_0_phone.png"
       },
-      "status": "pass"
+      "status": "fail"
     },
     {
       "pair": {
         "reference": "..\\bitmaps_reference\\backstop_default_Blog_Homepage_0_document_1_tablet.png",
-        "test": "..\\bitmaps_test\\20191121-181548\\backstop_default_Blog_Homepage_0_document_1_tablet.png",
+        "test": "..\\bitmaps_test\\20191121-182735\\backstop_default_Blog_Homepage_0_document_1_tablet.png",
         "selector": "document",
         "fileName": "backstop_default_Blog_Homepage_0_document_1_tablet.png",
         "label": "Blog Homepage",
@@ -40,16 +41,121 @@ report({
         "expect": 0,
         "viewportLabel": "tablet",
         "diff": {
-          "isSameDimensions": true,
+          "isSameDimensions": false,
           "dimensionDifference": {
-            "width": 0,
+            "width": -2048,
+            "height": -6663
+          },
+          "misMatchPercentage": "2.31",
+          "analysisTime": 483
+        },
+        "diffImage": "..\\bitmaps_test\\20191121-182735\\failed_diff_backstop_default_Blog_Homepage_0_document_1_tablet.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\backstop_default_Blog_Homepage_0_document_2_desktop.png",
+        "test": "..\\bitmaps_test\\20191121-182735\\backstop_default_Blog_Homepage_0_document_2_desktop.png",
+        "selector": "document",
+        "fileName": "backstop_default_Blog_Homepage_0_document_2_desktop.png",
+        "label": "Blog Homepage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "http://localhost:8000",
+        "referenceUrl": "",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": -4864,
+            "height": -2910
+          },
+          "misMatchPercentage": "3.83",
+          "analysisTime": 525
+        },
+        "diffImage": "..\\bitmaps_test\\20191121-182735\\failed_diff_backstop_default_Blog_Homepage_0_document_2_desktop.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\backstop_default_Blog_Post_0_document_0_phone.png",
+        "test": "..\\bitmaps_test\\20191121-182735\\backstop_default_Blog_Post_0_document_0_phone.png",
+        "selector": "document",
+        "fileName": "backstop_default_Blog_Post_0_document_0_phone.png",
+        "label": "Blog Post",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "http://localhost:8000/template",
+        "referenceUrl": "",
+        "expect": 0,
+        "viewportLabel": "phone",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": -640,
+            "height": -6775
+          },
+          "misMatchPercentage": "1.89",
+          "analysisTime": 202
+        },
+        "diffImage": "..\\bitmaps_test\\20191121-182735\\failed_diff_backstop_default_Blog_Post_0_document_0_phone.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\backstop_default_Blog_Post_0_document_1_tablet.png",
+        "test": "..\\bitmaps_test\\20191121-182735\\backstop_default_Blog_Post_0_document_1_tablet.png",
+        "selector": "document",
+        "fileName": "backstop_default_Blog_Post_0_document_1_tablet.png",
+        "label": "Blog Post",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "http://localhost:8000/template",
+        "referenceUrl": "",
+        "expect": 0,
+        "viewportLabel": "tablet",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": -2048,
+            "height": -3744
+          },
+          "misMatchPercentage": "3.06",
+          "analysisTime": 418
+        },
+        "diffImage": "..\\bitmaps_test\\20191121-182735\\failed_diff_backstop_default_Blog_Post_0_document_1_tablet.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\backstop_default_Blog_Post_0_document_2_desktop.png",
+        "test": "..\\bitmaps_test\\20191121-182735\\backstop_default_Blog_Post_0_document_2_desktop.png",
+        "selector": "document",
+        "fileName": "backstop_default_Blog_Post_0_document_2_desktop.png",
+        "label": "Blog Post",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "http://localhost:8000/template",
+        "referenceUrl": "",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": -2304,
             "height": 0
           },
-          "misMatchPercentage": "0.00",
-          "analysisTime": 422
-        }
+          "misMatchPercentage": "9.91",
+          "analysisTime": 555
+        },
+        "diffImage": "..\\bitmaps_test\\20191121-182735\\failed_diff_backstop_default_Blog_Post_0_document_2_desktop.png"
       },
-      "status": "pass"
+      "status": "fail"
     }
   ],
   "id": "backstop_default"
