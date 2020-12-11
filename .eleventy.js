8703807fcb3d749d80ff18f179a009aa4922574f
Amy Kapernick <amy@kapers.dev>
2020-12-11 14:20:49 +0800

    added rss feeds

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,16 @@
 require('dotenv').config()
 
-const slug = require('./src/filters/slug')
+const slug = require('./src/filters/slug'),
+pluginRss = require('@11ty/eleventy-plugin-rss')
 
 module.exports = (eleventyConfig) => {
 	eleventyConfig.setBrowserSyncConfig({
 		notify: true,
 		watch: true,
 	})
+
+	// Plugins
+	eleventyConfig.addPlugin(pluginRss)
 
 	// Filters
 	eleventyConfig.addFilter('slug', slug)
