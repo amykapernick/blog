0ddb66624335d144db73aaa13b3dea3dce343fa3
Amy Kapernick <amy@kapers.dev>
2021-02-23 14:18:44 +0800

    fixed absolute replacements

--- site/utils/markdown/absoluteImages.js
+++ site/utils/markdown/absoluteImages.js
@@ -1,5 +1,5 @@
 const replace = (content) => {
-	return content.replace(/"\/img\//gi, `${process.env.SITE_URL}/img/`)
+	return content.replace(/"\/img\//gi, `"${process.env.SITE_URL}/img/`)
 }
 
 module.exports = replace
