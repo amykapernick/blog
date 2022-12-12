d592e66d1b3f31ad3f3bacf4a2160789a46ffdc9
Amy Kapernick <amy@kapers.dev>
2022-12-12 14:01:23 +0800

    checking id

--- _actions/netlify_deploy.sh
+++ _actions/netlify_deploy.sh
@@ -15,7 +15,7 @@ echo '$NETLIFY_SITE_ID'
 
 echo "${NETLIFY_SITE_ID}"
 
-COMMAND='netlify deploy --debug --build --site $NETLIFY_SITE_ID --auth $NETLIFY_AUTH_TOKEN --json --message "Deploying from GitHub Actions"'
+COMMAND="netlify deploy --debug --build --site ${NETLIFY_SITE_ID} --auth ${NETLIFY_AUTH_TOKEN} --json --message 'Deploying from GitHub Actions'"
 
 if [ "$prod" = "true" ]; then
     COMMAND="$COMMAND --prod"
