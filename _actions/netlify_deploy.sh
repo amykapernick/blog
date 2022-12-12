9531db99d59b85b1e6697953ad7c0789ee65e90a
Amy Kapernick <amy@kapers.dev>
2022-12-12 13:14:04 +0800

    testing changing command

--- _actions/netlify_deploy.sh
+++ _actions/netlify_deploy.sh
@@ -9,7 +9,7 @@ done
 
 netlify -v
 
-COMMAND='netlify deploy --debug --build --site $NETLIFY_SITE_ID --auth $NETLIFY_AUTH_TOKEN --json --message "Deploying from GitHub Actions"'
+COMMAND='netlify deploy --debug --build --site ${NETLIFY_SITE_ID} --auth ${NETLIFY_AUTH_TOKEN} --json --message "Deploying from GitHub Actions"'
 
 if [ "$prod" = "true" ]; then
     COMMAND="$COMMAND --prod"
