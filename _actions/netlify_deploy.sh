aab28df1d8ca701c4b8cb35b2830673c3b7124ad
Amy Kapernick <amy@kapers.dev>
2022-12-12 14:14:41 +0800

    switched message

--- _actions/netlify_deploy.sh
+++ _actions/netlify_deploy.sh
@@ -7,7 +7,7 @@ do
     esac
 done
 
-MESSAGE = "Deploying from GitHub Actions"
+MESSAGE="Deploying from GitHub Actions"
 
 COMMAND="netlify deploy --debug --build --site ${NETLIFY_SITE_ID} --auth ${NETLIFY_AUTH_TOKEN} --json --message ${MESSAGE}"
 
