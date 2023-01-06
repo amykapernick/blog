59b6e36c9f78e2adb19257641c2fd7080dcbaa78
Amy Kapernick <amy@kapers.dev>
2023-01-06 11:39:16 +0800

    add netlify build and deploy

--- _actions/netlify_deploy.sh
+++ _actions/netlify_deploy.sh
@@ -1,25 +1,19 @@
 #!/bin/bash
 
-while getopts p: flag
-do
-    case "${flag}" in
-        p) prod=${OPTARG};;
-    esac
-done
+# Here we'll set the command to run, this is using the Netlify CLI using the deploy command. We'll pass in the environment variables for the site and auth token, and have set the output to come through as JSON
+# https://cli.netlify.com/commands/deploy
+COMMAND="netlify deploy --build --site ${NETLIFY_SITE_ID} --auth ${NETLIFY_AUTH_TOKEN} --json"
 
-COMMAND="netlify deploy --debug --build --site ${NETLIFY_SITE_ID} --auth ${NETLIFY_AUTH_TOKEN} --json"
-
-if [ "$prod" = "true" ]; then
-    COMMAND="$COMMAND --prod"
-fi
-
+# Next we'll run the command, and save the output in another variable so we can access it
 OUTPUT=$($COMMAND)
 
+# To parse the output from Netlify, the jq package allows us to fetch the different properties and save them as individual variables.
+# https://stedolan.github.io/jq/
 NETLIFY_URL=$(jq -r '.deploy_url' <<<"${OUTPUT}")
 NETLIFY_LOGS=$(jq -r '.logs' <<<"${OUTPUT}")
 DEPLOY_ID=$(jq -r '.deploy_id' <<<"${OUTPUT}")
 SITE_NAME=$(jq -r '.site_name' <<<"${OUTPUT}")
 
-echo $NETLIFY_URL
-
+# Lastly we'll save the Netlify preview URL as an output parameter for the workflow step, so we can access it in future steps, eg. to add it as a comment on our PR
+# https://docs.github.com/en/actions/using-workflows/workflow-commands-for-github-actions#setting-an-output-parameter
 echo "NETLIFY_URL=${NETLIFY_URL}" >> $GITHUB_OUTPUT
